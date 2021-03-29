d5260f5a1ed13c69e87e045d7c176bd7164d603a
Brian Ross <brian.ross@students.makeschool.com>
2021-03-29 19:43:34 -0400

    working gif search

--- app.js
+++ app.js
@@ -11,15 +11,34 @@ const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars');
 
-//routes 
-app.get('/', (req, res) => {
-    console.log(req.query)
 
-    res.render('home')
+//tenor api
+const Tenor = require("tenorjs").client({
+    // Replace with your own key
+    "Key": "J000LT5ED092", // https://tenor.com/developer/keyregistration
+    "Filter": "off", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
 });
 
+//routes 
+app.get('/', (req, res) => {
+    // Handle the home page when we haven't queried yet
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+    // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+    Tenor.Search.Query(term, "10")
+        .then(response => {
+            // store the gifs we get back from the search
+            const gifs = response;
+            // pass the gifs as an object into the home page
+            res.render('home', { gifs })
+        }).catch(console.error);
+  })
 
-  app.get('/greetings/:name', (req, res) => {
+
+app.get('/greetings/:name', (req, res) => {
     // grab the name from the path provided
     const name = req.params.name;
     // render the greetings view, passing along the name
